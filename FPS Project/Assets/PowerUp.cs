8e18619da53c6dc877cacb0637e5466bff4fdfff
LouisPerry4281 <louisperry4281@gmail.com>
2024-01-11 06:13:26 -0800

    Added Rocket Power Up
    Added a rocket powerup so it becomes part of the level progression.

---
+++ FPS Project/Assets/PowerUp.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    [SerializeField] GameObject rocketLauncherReference;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check for player layer
+        if (other.gameObject.layer == 8)
+        {
+            EnableRocket();
+        }
+    }
+
+    private void EnableRocket()
+    {
+        rocketLauncherReference.SetActive(true);
+        Destroy(gameObject);
+    }
+}
