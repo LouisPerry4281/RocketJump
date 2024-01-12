cdffe36ca133fb7468b6b56e0781151a2ca47a67
LouisPerry4281 <louisperry4281@gmail.com>
2024-01-12 12:53:29 +0000

    Added Checkpoint System
    Added the checkpoint system for the player, currently enemies will not respawn.

---
+++ FPS Project/Assets/CheckPointManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointManager : MonoBehaviour
+{
+    Vector3 respawnPos;
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        respawnPos = player.transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            RespawnPlayer();
+    }
+
+    public void ChangeSpawnLocation(Vector3 newPos)
+    {
+        respawnPos = newPos;
+    }
+
+    public void RespawnPlayer()
+    {
+        player.transform.position = respawnPos;
+        //Add Enemy Spawn Later
+    }
+
+}
---
+++ FPS Project/Assets/CheckPointScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointScript : MonoBehaviour
+{
+    GameObject gameManager;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check for player later
+        if (other.gameObject.layer == 8)
+        {
+            gameManager.GetComponent<CheckPointManager>().ChangeSpawnLocation(transform.position);
+            Destroy(gameObject);
+        }
+    }
+}
