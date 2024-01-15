06727d4065483ebb030d82467b87e4c7618ca9c4
LouisPerry4281 <louisperry4281@gmail.com>
2024-01-15 03:49:29 +0000

    Implemented Player Health System
    Implemented a player health system that interacts with the enemy bullets and is scalable to add more hazards later.

---
+++ FPS Project/Assets/EnemyBullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    [SerializeField] float damage;
+
+    private void Start()
+    {
+        Invoke(nameof(DestroyThis), 5);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //If this collides with the player
+        if (other.gameObject.layer == 8)
+        {
+            other.gameObject.GetComponentInParent<PlayerHealth>().TakeDamage(damage);
+            DestroyThis();
+        }
+    }
+
+    void DestroyThis()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ FPS Project/Assets/PlayerHealth.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    GameManager gameManager;
+
+    [SerializeField] float maxHealth;
+    [SerializeField] float healingRate;
+    [SerializeField] float recoveryDelay;
+
+    float currentHealth;
+    bool fullHealth = true;
+
+    float outOfDangerTimer;
+    bool outOfDanger = true;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        OutOfDangerCheck();
+
+        if (outOfDanger && !fullHealth)
+        {
+            HealPlayer();
+            if (currentHealth >= maxHealth)
+            {
+                fullHealth = true;
+                currentHealth = maxHealth;
+            }
+        }
+    }
+
+    private void OutOfDangerCheck()
+    {
+        outOfDangerTimer -= Time.deltaTime;
+
+        if (outOfDangerTimer <= 0)
+        {
+            outOfDanger = true;
+        }
+
+        else
+            outOfDanger = false;
+    }
+
+    private void HealPlayer()
+    {
+        currentHealth += healingRate;
+    }
+
+    public void TakeDamage(float damageToTake)
+    {
+        print("Ouchies: " + damageToTake + " damage!");
+        currentHealth -= damageToTake;
+        outOfDangerTimer = recoveryDelay;
+
+        CheckHealth();
+    }
+
+    private void CheckHealth()
+    {
+        if (currentHealth <= 0)
+        {
+            gameManager.ScanKillTarget(gameObject);
+            currentHealth = maxHealth;
+        }
+
+        if (currentHealth < maxHealth)
+        {
+            fullHealth = false;
+        }
+    }
+}
